b99192a049cac9537eba80c2fe1c8b8a4202c974
Jamil Said <jamilsaid@hotmail.com>
2018-09-07 16:37:12 -0700

    Commit 9-07-18

---
+++ Python_code_challenges/binaryTreeSimpleTraversal.py
@@ -0,0 +1,44 @@
+
+""" binaryTreeSimpleTraversal
+Given a binary tree, return the level order traversal of its nodes' values. 
+(ie, from left to right, all stored in order in an array).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+
+[3, 9, 20, 15, 7]
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+from collections import deque
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        holder, tmp, result = deque([root]), []
+        while holder:
+            cur = holder.popleft()
+            result.append(cur.val)
+            if cur.left: holder.append(cur.left)
+            if cur.right: holder.append(cur.right)
+        return result
+
+
+#print(Solution().())
