e23fcdc965cca26afa4b1ac7d93e266311b327e2
Jamil Said <jamilsaid@hotmail.com>
2018-10-04 16:59:35 -0700

    Commit 10-4-18

--- Python_code_challenges/binaryTreeSimpleTraversal.py
+++ Python_code_challenges/binaryTreeSimpleTraversal.py
@@ -18,11 +18,11 @@ return its level order traversal as:
 """
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 from collections import deque
@@ -32,7 +32,7 @@ class Solution:
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        holder, tmp, result = deque([root]), []
+        holder, result = deque([root]), []
         while holder:
             cur = holder.popleft()
             result.append(cur.val)
@@ -40,5 +40,11 @@ class Solution:
             if cur.right: holder.append(cur.right)
         return result
 
+# Creating tree for test
+node = TreeNode(3)
+node.left = TreeNode(9)
+node.right = TreeNode(20)
+node.right.left = TreeNode(15)
+node.right.right = TreeNode(7)
 
-#print(Solution().())
+print(Solution().levelOrder(node)) #[3, 9, 20, 15, 7]
