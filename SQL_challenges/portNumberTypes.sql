a77857f770ba294ddbae1569167f0373b0ccd01f
owner <owner@debian10>
2019-11-16 16:04:36 -0800

    Commit 11-16-19

--- SQL_challenges/portNumberTypes.sql
+++ SQL_challenges/portNumberTypes.sql
@@ -1,10 +1,8 @@
 
--- MySQL and PostgreSQL tested
+/* portNumberTypes -- MySQL
+Given a table "portgroups" as following:
 
-/* portNumberTypes
-Given a table "groups" as following:
-
-    CREATE TABLE groups (
+    CREATE TABLE portgroups (
         port_number integer not null,
         group_type integer not null,
         value float not null,
@@ -33,16 +31,42 @@ your query should return the following result:
                7 |     1
 */
 
+/* Create database
+mysql -u root -p
+CREATE DATABASE portypes;
+use portypes;
+*/
+
+/* Create example table and populate it
+CREATE TABLE portgroups (
+    port_number integer not null,
+    group_type integer not null,
+    value float not null,
+    time timestamp not null
+);
+
+
+INSERT INTO portgroups (port_number, group_type, value, time) VALUES
+    (4, 3, 9.31, '2017-09-14 11:32:00'),
+    (4, 5, -24.42, '2017-09-14 11:17:03'),
+    (4, 3, 834.5, '2017-09-14 10:34:37'),
+    (7, 2, -31.27, '2017-09-14 14:45:06'),
+    (4, 8, 8.64, '2017-09-14 18:31:31');
+*/
+
 
 SELECT port_number, COUNT(DISTINCT group_type) AS types
-FROM groups
-GROUP BY port_number
-ORDER BY port_number ASC;
+    FROM portgroups
+    GROUP BY port_number
+    ORDER BY port_number;
+
 
 /*
+
  port_number | types 
 -------------+-------
            4 |     3
            7 |     1
+
 */
 
