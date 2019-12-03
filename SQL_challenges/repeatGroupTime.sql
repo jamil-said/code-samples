976d4ecc69e91d410f040c271caa59bdd0e9c762
owner <owner@debian10>
2019-12-02 20:54:47 -0800

    Commit 12-02-19

--- SQL_challenges/repeatGroupTime.sql
+++ SQL_challenges/repeatGroupTime.sql
@@ -1,10 +1,7 @@
+/* repeatGroupTime -- MySQL
+Given a table "grouptimes" as following:
 
--- MySQL and PostgreSQL tested
-
-/* repeatGroupTime
-Given a table "groups" as following:
-
-    CREATE TABLE groups (
+    CREATE TABLE grouptimes (
         group_number integer not null,
         value integer not null,
         time timestamp not null,
@@ -39,15 +36,46 @@ For the group_number 4, the latest value is 2 and the second latest value
 is 8, so the difference between them is -6. 
 */
 
+/* Create database
+mysql -u root -p
+CREATE DATABASE repeatgroup;
+USE repeatgroup;
+*/
 
-SELECT gr1.group_number, (gr1.value - gr2.value) AS value FROM groups gr1 
-JOIN groups gr2
+/* Create example table and populate it
+CREATE TABLE grouptimes (
+    group_number integer not null,
+    value integer not null,
+    time timestamp not null,
+    unique(group_number, time)
+);
+
+INSERT INTO grouptimes (group_number, value, time) VALUES
+    (4, 8, '2017-08-12 11:32:00'),
+    (5, -2, '2017-08-12 11:39:58'),
+    (4, 2, '2017-08-12 13:37:35'),
+    (4, 12, '2017-08-12 9:50:23'),
+    (6, 11, '2017-08-12 8:12:51'),
+    (6, -3, '2017-08-12 9:11:00');
+*/
+
+
+SELECT gr1.group_number, (gr1.value - gr2.value) AS value 
+FROM grouptimes gr1
+JOIN grouptimes gr2 
 ON gr1.group_number = gr2.group_number
-AND gr1.time = (SELECT time FROM groups tmp1 
-    WHERE tmp1.group_number=gr1.group_number ORDER BY time DESC LIMIT 1)
-AND gr2.time = (SELECT time FROM groups tmp2 
-    WHERE tmp2.group_number=gr2.group_number ORDER BY time DESC LIMIT 1 OFFSET 1)
-ORDER BY group_number ASC;
+AND gr1.time = (SELECT tmp1.time 
+    FROM grouptimes tmp1 
+    WHERE tmp1.group_number=gr1.group_number 
+    ORDER BY time DESC 
+    LIMIT 1)
+AND gr2.time = (SELECT tmp2.time 
+    FROM grouptimes tmp2
+    WHERE tmp2.group_number=gr2.group_number 
+    ORDER BY time DESC 
+    LIMIT 1 
+    OFFSET 1)
+ORDER BY group_number;
 
 
 /* expected result
