52eb1b6510d75a4be4db7a8652ff92963135d1d4
owner <owner@debian10>
2019-12-29 15:43:20 -0800

    Commit 12-29-19

--- Python_code_challenges/fourLetterWords.py
+++ Python_code_challenges/fourLetterWords.py
@@ -1,31 +1,44 @@
-
-""" fourLetterWords -- 4 minutes
+""" fourLetterWords
 Write a function that takes a sentence and returns the number of four letter
 words it contains. Dont worry about handling punctuation 
 """
 
-def four_letter_words(sentence):
-    result = 0
-    for i in sentence.split():
-        if len(i) == 4: result += 1
-    return result
+# If punctuation doesn't matter (as problem states):
+def four_letter_words(s):
+    count = 0
+    arr = s.split()
+    for w in arr:
+        if len(w) == 4:
+            count += 1
+    return count
 
-""" #alternative, checks if all chars are alpha in len 4 strings
-def four_letter_words(sentence):
-    result, temp = 0, 0
-    for i in sentence.split():
-        for j in i:
-            if j.isalpha(): temp += 1
-        if temp == 4: result += 1
+
+# Alternative: if punctuation matters and have to be accounted for:
+import re
+def four_letter_words(s):
+    count = 0
+    arr = re.split(r'\W+', s)
+    for w in arr:
+        if len(w) == 4:
+            count += 1
+    return count
+    
+    
+#alternative, if punctuation matters (slower but more specific):
+def four_letter_words(s):
+    res, temp = 0, 0
+    for w in s.split():
+        for c in w:
+            if c.isalpha(): 
+                temp += 1
+        if temp == 4: 
+            res += 1
         temp = 0
-    return result
-"""
+    return res
+
 
 print(four_letter_words("This sentence is fine")) # 2
 print(four_letter_words("So is this one")) # 1
 print(four_letter_words("Hello")) # 0
-print(four_letter_words("This sentence is differemt, it has punctuation!!!")) # 1
-#print(four_letter_words("This one is tricky !!!!")) # 1 this one only if count non alpha,
-
-
-
+print(four_letter_words("This sentence is different, it has punctuation!!!")) # 1
+print(four_letter_words("This one is tricky !!!!")) # 1 #this test is for variants where punctuation matters
