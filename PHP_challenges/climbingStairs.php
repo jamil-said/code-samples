134b9496c6d0343ab6f70ea26838e3647fa5ce1d
owner <owner@debian10>
2020-01-20 22:00:34 -0800

    Commit 1-20-20

--- PHP_challenges/climbingStairs.php
+++ PHP_challenges/climbingStairs.php
@@ -1,4 +1,6 @@
-""" climbingStairs
+<?php
+
+/* climbingStairs
 You are climbing a stair case. It takes n steps to reach to the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways 
@@ -24,16 +26,21 @@ Explanation:  There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
-"""
+*/
 
 
-class Solution:
-    def climbStairs(self, n):
-        a, b = 1, 1
-        for i in range(n):
-            a, b = b, a+b
-        return a
-            
+class Solution{
+    function climbStairs($n) {
+        $a = 1;
+        $b = 1;
+        foreach(range(1, $n) as $i) {
+            $tmpA = $a;
+            $a = $b;
+            $b += $tmpA;
+        }
+        return $a;
+    }
+}            
 
-print(Solution().climbStairs(6)) #13
-print(Solution().climbStairs(38)) #63245986
+echo Solution::climbStairs(6), "\n"; #13
+echo Solution::climbStairs(38), "\n"; #63245986
