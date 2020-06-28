676b3201dc9cb1fe0860b4f9d774184351782a1d
owner <owner@debian10>
2020-06-28 01:00:49 -0700

    Commit 6-28-20

--- Python_code_challenges/mazeSmallestPathToTreasure.py
+++ Python_code_challenges/mazeSmallestPathToTreasure.py
@@ -1,4 +1,3 @@
-
 """ mazeSmallestPathToTreasure
 Given a maze (a 2d array), and considering that you can traverse the maze 
 where the value is 1 and cannot traverse it when the value is 0, find the 
@@ -19,34 +18,52 @@ would return 3, as one would have to traverse the maze elements (0,0),
 (1,0),(2,0) to arrive at the treasure located at (2,1)
 """
 
+# I think this solution is right, but it was not tested with a large test case
 def mazeFind(maze):
-    if not maze or not maze[0]: return -1
-    if maze[0][0] == 5: return 0
-    ok, result = set(), []
-    for i in range(len(maze)):
-        for j in range(len(maze[0])):
-            if maze[i][j] == 1: ok.add((i,j))
-            elif maze[i][j] == 5: tgt = (i,j)
-    calc([], ok, tgt, set(), 0, 0, result)
-    return sorted(result)[0]-1 if result else -1
+    if not maze or not maze[0]: 
+        return -1
+    result = []
+    calc([], maze, [], 0, 0, result)
+    return result[0] if result else -1
 
-def calc(path, ok, tgt, vis, i, j, result):
-    if (i,j) == tgt: result.append(len(path+[1]))
-    elif (i,j) not in ok or (i,j) in vis: return
+def calc(path, maze, vis, i, j, result):
+    if (i,j) in vis: 
+        return
+    elif (i >= len(maze) or i < 0) or (j >= len(maze[0]) or j < 0) or (maze[i][j] == 0):
+        return 
+    elif result and len(path) >= result[0]:
+        return
+    if maze[i][j] == 5: 
+        if result:
+            result[0] = min(len(path),result[0])
+        else:
+            result.append(len(path))
     else:
-        vis.add((i,j))
-        calc(path+[1], ok, tgt, vis, i+1, j, result)
-        calc(path+[1], ok, tgt, vis, i, j+1, result)
-        calc(path+[1], ok, tgt, vis, i-1, j, result)
-        calc(path+[1], ok, tgt, vis, i, j-1, result)
+        calc(path+[(i,j)], maze, vis+[(i,j)], i+1, j, result)
+        calc(path+[(i,j)], maze, vis+[(i,j)], i, j+1, result)
+        calc(path+[(i,j)], maze, vis+[(i,j)], i-1, j, result)
+        calc(path+[(i,j)], maze, vis+[(i,j)], i, j-1, result)
             
 
 print(mazeFind([[1, 0, 0], [1, 0, 0], [1, 5, 1]])) #3
-print(mazeFind([[1, 1, 1, 1], [0, 1, 1, 1], [0, 1, 0, 1], [1, 1, 5, 1], 
-[0, 0, 1, 1]])) #5
+print(mazeFind([[1, 1, 1, 1], 
+                [0, 1, 1, 1], 
+                [0, 1, 0, 1], 
+                [1, 1, 5, 1], 
+                [0, 0, 1, 1]])) #5
 print(mazeFind([[0, 0, 0], [1, 0, 0], [1, 5, 1]])) #-1
-print(mazeFind([[5, 0, 0], [1, 0, 0], [1, 5, 1]])) # 0
+print(mazeFind([[5, 0, 0], [1, 0, 0], [1, 0, 1]])) # 0
 print(mazeFind([[]])) # -1
 print(mazeFind([])) # -1
-
-
+print(mazeFind([[1,1,1,1,1,1],
+                [1,0,0,0,1,1],
+                [1,0,5,0,1,1],
+                [1,1,1,0,1,1],
+                [1,1,1,1,1,1],
+                [1,1,1,1,1,1]])) #6
+print(mazeFind([[1,1,1,1,1,1],
+                [1,0,0,0,1,1],
+                [1,0,5,0,1,1],
+                [1,0,1,0,1,1],
+                [1,1,1,0,1,1],
+                [1,1,1,1,1,1]])) #8
