4cb972f38f44f289ac1c0d8be9a241cc9ee3175c
owner <owner@debian10>
2021-01-16 22:52:25 -0800

    Commit 01-16-21

--- SQL/SQL_challenges/habitatArea.sql
+++ SQL/SQL_challenges/habitatArea.sql
@@ -39,7 +39,6 @@ area
 
 CREATE PROCEDURE habitatArea()
 BEGIN
-    SELECT 
-    ST_AREA(ST_ConvexHull(ST_GeomFromText(CONCAT('MULTIPOINT(', GROUP_CONCAT(x, " ", y), ')'))))
+    SELECT ST_AREA(ST_ConvexHull(ST_GeomFromText(CONCAT('MULTIPOINT(', GROUP_CONCAT(x, " ", y), ')'))))
     AS area FROM places;
 END
